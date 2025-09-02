05c88d120b37fd43b0edb0aab7b14496d4a9ac89
unknown <barasoft@barabdonline.xyz>
2025-09-02 17:37:10 +0600

    update

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -50,7 +50,7 @@
   <body class="login-page bg-body-secondary">
     <div class="login-box">
       <div class="login-logo">
-        <a href="#"><b>Education Management System</a>
+        <a href="#"><b>Education Management System..</a>
       </div>
       <!-- /.login-logo -->
       <div class="card">
