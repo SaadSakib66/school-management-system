d04806278c791294c3c425d27dad260864642728
unknown <barasoft@barabdonline.xyz>
2025-09-21 09:20:53 +0600

    hellow

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -314,6 +314,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
   // Smooth anchor scroll
+  
   document.querySelectorAll('a[href^="#"]').forEach(a=>{
     a.addEventListener('click', e=>{
       const id = a.getAttribute('href');
