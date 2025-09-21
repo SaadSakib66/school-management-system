c137806907608a62d28c16774017bfda4fce510f
unknown <barasoft@barabdonline.xyz>
2025-09-21 10:34:13 +0600

    hellow

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -258,6 +258,7 @@
         </div>
       </div>
 
+      
       <div class="accordion-item">
         <h2 class="accordion-header" id="q2">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#a2">
@@ -314,7 +315,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
   // Smooth anchor scroll
-  
+
   document.querySelectorAll('a[href^="#"]').forEach(a=>{
     a.addEventListener('click', e=>{
       const id = a.getAttribute('href');
