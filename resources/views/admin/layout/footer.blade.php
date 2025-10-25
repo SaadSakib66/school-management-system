7881831c2ee501c412d9221faa5e29740bfb030c
SaadSakib66 <barasoft@barabdonline.xyz>
2025-10-25 12:17:50 +0600

    footer fixing

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="app-footer">
     <!--begin::To the end-->
-    <div class="float-end d-none d-sm-inline">Anything you want</div>
+
     <!--end::To the end-->
     <!--begin::Copyright-->
     <strong>
     Copyright &copy; 2025-2026&nbsp;
-    <a href="https://barabdonline.xyz/" class="text-decoration-none" style="color: black;">B.A.R & Associates</a>.
+    <a href="https://barabdonline.xyz/" class="text-decoration-none" style="color: black;">B.A.R & Associates</a>(<span style="color: red">bara</span><span style="color: green">bd</span>)
     </strong>
     All rights reserved.
     <!--end::Copyright-->
